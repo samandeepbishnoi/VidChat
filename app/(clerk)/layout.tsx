03f6f2abacbc68027a2b02ace09d56f6d3db9e99
samandeepbishnoi <samandeepbishnoi@gamil.com>
2024-12-17 12:12:23 +0530

    authentcation

---
+++ app/(clerk)/layout.tsx
@@ -0,0 +1,4 @@
+const ClerLayout = ({children} : {children: React.ReactNode})=> {
+    return <div className="h-screen flex items-center justify-center" >{children}</div>
+}
+export default ClerLayout
